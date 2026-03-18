e2c13ddc759429e3c1fc76ae7e5cc9fb24b48773
Julio Cesar <jcesar206@hotmail.com>
2026-03-18 09:43:10 -0600

    Se sube archivo translations.js como diccionario de la app

---
+++ src/i18n/translations.js
@@ -0,0 +1,14 @@
+export const translations = {
+	es: {
+		welcome: "Bienvenido",
+		login: "Iniciar sesión",
+		logout: "Cerrar sesión",
+		products: "Productos"
+	},
+	en: {
+		welcome: "Welcome",
+		login: "Login",
+		logout: "Logout",
+		products: "Products"
+	}
+};

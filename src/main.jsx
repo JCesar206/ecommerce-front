7bf906004bdaeb4a3f1994afa2d45f1d86bf8d17
Julio Cesar <jcesar206@hotmail.com>
2026-03-19 05:28:23 -0600

    Se actualiza archivo main.jsx manejo de envoltura de contextos y routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from './App.jsx';
-import './index.css';
-import { AuthProvider } from './context/AuthContext.jsx';
-import { ThemeProvider } from './context/ThemeContext.jsx';
-import { LanguageProvider } from './context/LanguageContext.jsx';
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { LanguageProvider } from "./context/LanguageContext.jsx";
+import { CartProvider } from "./context/CartContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
       <LanguageProvider>
         <AuthProvider>
-          <App/>
+          <CartProvider>
+            <App/>
+          </CartProvider>
         </AuthProvider>
       </LanguageProvider>
     </ThemeProvider>
